470256d042d08a7ece859eda75a0c11282a801af
Asen_G <assen.georgiew@gmail.com>
2018-04-08 01:01:38 +0200

    decorators - just the beginning

---
+++ decorators.py
@@ -0,0 +1,24 @@
+'''This is the doc string of the decorators.py moduleself.
+And we use tripple quoting, because it allows the string to contain line breaks'''
+
+from functools import wraps
+
+def first_decorator_txt(deco_txt):
+    def first_decorator(func):
+        @wraps(func)
+        def wrapper_func(var):
+            return 'Nqkakva {0} kaza : {1}'.format(deco_txt, func(var))
+        return wrapper_func
+    return first_decorator
+
+
+
+@first_decorator_txt('patka')
+def double_text(text_to_print):
+    '''This is the documentation string of the double_text function '''
+    return text_to_print + text_to_print
+
+print(double_text('edno_'))
+print(double_text.__name__)
+print(double_text.__doc__)
+print(__doc__)
