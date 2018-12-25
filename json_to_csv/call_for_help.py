0d3c5ffcab6e7da10065c0701e2bb4091eb1825c
Asen_G <assen.georgiew@gmail.com>
2018-12-25 23:59:54 +0200

    json_to_csv_v6_incomplete_more_pythonic

---
+++ json_to_csv/call_for_help.py
@@ -0,0 +1,33 @@
+test_json = {"records": "563",
+             "campaign": [{"id":"2027698","title":"OMFM Client Jet Charter 3 - Smart Inbox intro (average) GLOCKTEST alignable","status":"Completed","scheduledate":"December 13, 2018, 11:23:00 am","starttime":"December 13, 2018, 11:24:24 am","endtime":"December 13, 2018, 11:26:12 am","lastupdate":{},"sent":"41","views":"4","clicks":"3","optouts":"0","conversions":"0"},
+                         {"id":"2027689","title":"OMFM Client Jet Charter 3 - Smart Inbox intro (average) GLOCKTEST no links","status":"Completed","scheduledate":"December 13, 2018, 10:29:00 am","starttime":"December 13, 2018, 10:30:24 am","endtime":"December 13, 2018, 10:32:10 am","lastupdate":{},"sent":"41","views":"1","clicks":"0","optouts":"0","conversions":"0"}]
+             }
+
+
+def data_multiplication(initial_nested_data):
+    out = [{}]
+
+    def data_multiplication_(nested_data):
+        if isinstance(nested_data, list) and len(nested_data) > 0:
+            base_dic = out[-1]
+            print('match')
+            print(base_dic)
+            for x, _ in enumerate(nested_data):
+                data_multiplication_(nested_data[x])
+                print(base_dic)
+                out.append(base_dic)
+
+        elif isinstance(nested_data, dict) or len(nested_data) == 0:
+            for x in nested_data:
+                if (isinstance(nested_data[x], list) or isinstance(nested_data[x], dict)) and len(nested_data[x]) > 0:
+                    data_multiplication_(nested_data[x])
+                else:
+                    out[-1][x] = nested_data[x]
+
+    data_multiplication_(initial_nested_data)
+
+    return out
+
+
+if __name__ == '__main__':
+    data_multiplication(test_json)
--- json_to_csv/json_to_csv_v6.py
+++ json_to_csv/json_to_csv_v6.py
@@ -1,5 +1,3 @@
-# https://medium.com/@gis10kwo/converting-nested-json-data-to-csv-using-python-pandas-dc6eddc69175
-
 import json
 import pandas as pd
 
@@ -7,10 +5,10 @@ import pandas as pd
 def parse():
     with open('./input.txt', 'r', encoding='utf-8') as f:
         in_json = json.load(f)
-        print(in_json)
+        # print(in_json)
         in_json = data_multiplication(in_json)
         parsed_json_df = pd.io.json.json_normalize(in_json)
-        print(parsed_json_df)
+        # print(parsed_json_df)
 
     return parsed_json_df
 
@@ -23,27 +21,27 @@ def data_multiplication(initial_nested_data):
     out = [{}]
 
     def data_multiplication_(nested_data):
-        if type(nested_data) == list:
+        if type(nested_data) == list and len(nested_data) > 0:
             n = 0
             base_dic = out[len(out) - 1]
+            print('match')
+            print(out)
             for _ in nested_data:
                 data_multiplication_(nested_data[n])
-                out.append(base_dic)
+                #breakpoint()
+                out.append({})  # base_dic)
+                #breakpoint()
+                print(base_dic)
                 n += 1
 
-        elif type(nested_data) == dict:
+        elif type(nested_data) == dict or len(nested_data) == 0:
 
             for x in nested_data:
-                if type(nested_data[x]) == list or type(nested_data[x]) == dict:
-                    # base_dic = out[len(out) - 1]
-                    # out.append(base_dic)
+                if (type(nested_data[x]) == list or type(nested_data[x]) == dict) and len(nested_data[x]) > 0:
                     data_multiplication_(nested_data[x])
 
                 else:
-                    out[len(out)-1][x] = nested_data[x]
-
-        # else:
-        #    out[0].append(nested_data)
+                    out[len(out) - 1][x] = nested_data[x]
 
     data_multiplication_(initial_nested_data)
 
