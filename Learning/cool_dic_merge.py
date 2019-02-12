20645dc65c9e763eacec9850b847082805a96bea
Asen_G <assen.georgiew@gmail.com>
2019-02-12 18:06:58 +0200

    Chio codes - generator update

---
+++ Learning/cool_dic_merge.py
@@ -0,0 +1,6 @@
+x = {'both1': 1, 'both2': 2, 'only_x': 100}
+y = {'both1': 10, 'both2': 20, 'only_y': 200}
+
+print({k: x.get(k, 0) + y.get(k, 0) for k in set(x)})
+print({k: x.get(k, 0) + y.get(k, 0) for k in set(x) & set(y)})
+print({k: x.get(k, 0) + y.get(k, 0) for k in set(x) | set(y)})
